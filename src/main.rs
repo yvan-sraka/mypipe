477fc182cc3d743212b0948ed606ff512d644838
quantumsheep <nathanael.dmc@outlook.fr>
2021-05-12 18:05:44 +0200

    Allow command arguments to be used in `--in` and `--out`

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,18 @@ fn main() -> io::Result<()> {
     let in_command = matches.value_of("in").unwrap();
     let out_command = matches.value_of("out").unwrap();
 
-    let in_command_child = Command::new(in_command).stdout(Stdio::piped()).spawn()?;
-    let out_command_output = Command::new(out_command)
+    let mut in_args = shlex::split(in_command)
+        .unwrap_or_else(|| panic!("`--in` argument should be a valid command"));
+    let mut out_args = shlex::split(out_command)
+        .unwrap_or_else(|| panic!("`--out` argument should be a valid command"));
+
+    let in_command_child = Command::new(in_args.remove(0))
+        .args(in_args)
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let out_command_output = Command::new(out_args.remove(0))
+        .args(out_args)
         .stdin(in_command_child.stdout.unwrap())
         .output()?;
 
