2c92c81ca3eab571694dfa22a461225a2ae90d6d
quantumsheep <nathanael.dmc@outlook.fr>
2021-05-12 19:16:11 +0200

    Export command validity check for `--in` and `--out`

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ use std::{
     process::{Command, Stdio},
 };
 
+fn is_valid_command(command: &str) -> Result<(), String> {
+    match shlex::split(command) {
+        None => Err(format!("invalid command")),
+        _ => Ok(()),
+    }
+}
+
 fn main() -> io::Result<()> {
     let matches = App::new("MasterMind")
         .args(&[
@@ -12,28 +19,23 @@ fn main() -> io::Result<()> {
                 .short('i')
                 .about("Input")
                 .required(true)
-                .takes_value(true),
+                .takes_value(true)
+                .validator(is_valid_command),
             Arg::new("out")
                 .long("out")
                 .short('o')
                 .about("Output")
                 .required(true)
+                .takes_value(true)
                 .multiple_occurrences(true)
-                .takes_value(true),
+                .validator(is_valid_command),
         ])
         .get_matches();
 
     let in_command = matches.value_of("in").unwrap();
     let out_commands: Vec<_> = matches.values_of("out").unwrap().collect();
 
-    let mut in_args = shlex::split(in_command)
-        .unwrap_or_else(|| panic!("`--in` argument should be a valid command"));
-
-    for i in 0..out_commands.len() {
-        if shlex::split(out_commands[0].clone()).is_none() {
-            panic!("`--out[{}]` argument should be a valid command", i)
-        }
-    }
+    let mut in_args = shlex::split(in_command).unwrap();
 
     let mut command_child = Command::new(in_args.remove(0))
         .args(in_args)
