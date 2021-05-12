0be79ac821c0dbe35c30af235a1cdad4c6dfa1c3
Stanislas Lange <slange@scaleway.com>
2021-05-12 16:26:40 +0000

    Simple, efficace

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::App;
+use duct::cmd;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("mypipe")
+        .version("1.0")
+        .about("Pipe one command into another")
+        .arg("-i, --in=[COMMAND] 'command'")
+        .arg("-o, --out=[COMMAND] 'command'")
+        .get_matches();
+
+    let stdout = cmd!(matches.value_of("in").unwrap())
+        .pipe(cmd!(matches.value_of("out").unwrap()))
+        .read()
+        .unwrap();
+
+    println!("{}", stdout);
 }
