05b51aeb276a5d50d40f59b7e677eca2568a62a6
Remaetanju <remibraizet@gmail.com$>
2021-05-16 17:39:59 +0200

    working execution and pipe

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Arg, App};
+use std::io::{self, Write};
 
 fn main() {
     let matches = App::new("mypipe")
@@ -17,7 +18,23 @@ fn main() {
 
     let input = matches.value_of("in").unwrap();
     let output = matches.value_of("out").unwrap();
+    let arg = "-c";
 
-    println!("in: {}", input);
-    println!("out: {}", output);
+    // executing left side command
+    let left_cmd_processed = std::process::Command::new("sh")
+        .arg(arg)
+        .arg(input)
+        .stdout(std::process::Stdio::piped())
+        .spawn()
+        .expect("error");
+
+    let right_cmd_processed = std::process::Command::new("sh")
+        .arg(arg)
+        .arg(output)
+        .stdin(std::process::Stdio::from(Some(left_cmd_processed).map_or(std::process::Stdio::inherit(),
+            | child_proc: std::process::Child | std::process::Stdio::from(child_proc.stdout.unwrap()))))
+        .output()
+        .expect("error");
+
+    io::stdout().write_all(&right_cmd_processed.stdout).unwrap();
 }
