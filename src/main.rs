69cf027dd5bb5c91dbd225740017301826ac1e23
Remaetanju <remibraizet@gmail.com$>
2021-05-16 17:00:28 +0200

    command line arg and cargo.toml clap dep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("mypipe")
+        .version("0.1.0")
+        .author(" rema ")
+        .about("Teaches argument parsing")
+        .arg(Arg::with_name("in")
+            .long("in")
+            .takes_value(true)
+            .help("input of pipe"))
+        .arg(Arg::with_name("out")
+            .long("out")
+            .takes_value(true)
+            .help("ouput of pipe"))
+        .get_matches();
+
+    let input = matches.value_of("in").unwrap();
+    let output = matches.value_of("out").unwrap();
+
+    println!("in: {}", input);
+    println!("out: {}", output);
 }
