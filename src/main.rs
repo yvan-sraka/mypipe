cda23eccf8d9883bae7178e2398aab709c548135
JeremTer <ternisien09@gmail.com>
2021-05-16 15:27:26 +0200

    😎

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::process::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+    //
+    // println!("{}", args.len());
+    if args.len() != 5 {
+        println!("Wrong arguments number");
+        return;
+    }
+
+    let arg_in = &args[2];
+    let arg_out = &args[4];
+
+    // println!("{}", arg_in);
+    // println!("{}", arg_out);
+
+    let first_command = Command::new(arg_in)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Argument in not recognized");
+
+    Command::new(arg_out)
+        .stdin(first_command.stdout.unwrap())
+        .spawn()
+        .expect("First command result incorrect");
 }
