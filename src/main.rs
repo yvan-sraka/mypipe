fe2eb0d46ec7829cac5b4f62819f928bdffe0598
Aymeric Larvet <aymeric.larvet@gmail.com>
2021-05-17 16:12:58 +0200

    mypipe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
-}
+use clap::{App, Arg};
+use std::{ process::{Command, Stdio}, io, char };
+
+fn main() -> io::Result<()> {
+    let matches = App::new("mypipe")
+        .args(&[
+            Arg::new("in")
+                .long("in")
+                .short('i')
+                .about("Input")
+                .required(true)
+                .takes_value(true),
+            Arg::new("out")
+                .long("out")
+                .short('o')
+                .about("Output")
+                .required(true)
+                .takes_value(true),
+        ])
+        .get_matches();
+
+    let input = matches.value_of("in").unwrap();
+    let output = matches.value_of("out").unwrap();
+
+    let input_output = Command::new(input).stdout(Stdio::piped()).spawn()?;
+    let output_output = Command::new(output).stdin(input_output.stdout.unwrap()).output()?;
+
+    println!("{}", String::from_utf8_lossy(&output_output.stdout));
+
+    Ok(())
+}  
